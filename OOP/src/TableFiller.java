f4c2a0ae72a888bb6f66ac04d34d65760c73c8ad
Sez-A <67644402+Sez-A@users.noreply.github.com>
2022-05-03 19:20:12 +0300

    Input validations

--- OOP/src/TableFiller.java
+++ OOP/src/TableFiller.java
@@ -3,8 +3,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TableFiller {
+    private final String VALID_STRING_PATTERN = "^\"[A-Za-z]+\"$";
+    private final String VALID_NUMBER_PATTERN = "^-*[0-9]+.*[\\d+*]$";
+    private final String VALID_FORMULA_PATTERN = "^= ([\\d]+)*([A-Z][\\d]+[A-Z][\\d]+)* [+\\-*\\/] ([\\d]+)*([A-Z][\\d]+[A-Z][\\d]+)*$";
+
     public void fill(Table table, int row, String info) {
         if (table.getContent().size() == row) {
             ArrayList<String> currentRow = new ArrayList<>(List.of(info));
@@ -21,9 +27,17 @@ public class TableFiller {
 
         int rowCnt = 0;
         while (scanner.hasNextLine()) {
-            String[] tokens = scanner.nextLine().split(", ");
+            String[] tokens = scanner.nextLine().trim().split(",");
+            if (tokens.length == 0) {
+                fill(table, rowCnt, "");
+                rowCnt++;
+                continue;
+            }
             for (int i = 0; i < tokens.length; i++) {
                 String cellInfo = tokens[i].trim();
+                if (inputIsInvalid(cellInfo)) {
+                    // TODO: 5/3/2022 Throw appropriate exception
+                }
                 if (cellInfo.contains("\\\"")) {
                     cellInfo = cellInfo.replace("\\\"", "");
                 }
@@ -31,5 +45,10 @@ public class TableFiller {
             }
             rowCnt++;
         }
+    }
+
+    private boolean inputIsInvalid(String data) {
+        // TODO Check whether data is valid!
+        return false;
     }
 }
